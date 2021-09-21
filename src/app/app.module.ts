49ca7da1c36ec823cfdf0fd14fae2bb4c32f3b75
brayangomez08 <gomezmancobrayanalexander@gmail.com>
2021-09-21 14:20:57 -0500

    Project start - IngresoEgresoApp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { IncomeExpensesComponent } from './income-expenses/income-expenses.component';
+import { StatisticsComponent } from './income-expenses/statistics/statistics.component';
+import { DetailComponent } from './income-expenses/detail/detail.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    DashboardComponent,
+    IncomeExpensesComponent,
+    StatisticsComponent,
+    DetailComponent,
+    FooterComponent,
+    NavbarComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/income-expenses/income-expenses.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-income-expenses',
+  templateUrl: './income-expenses.component.html',
+  styles: [
+  ]
+})
+export class IncomeExpensesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
