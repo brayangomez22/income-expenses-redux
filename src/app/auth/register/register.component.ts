cf016e916f364af39ad394e09fa14a8b809fcd64
brayangomez08 <gomezmancobrayanalexander@gmail.com>
2021-09-22 15:57:39 -0500

    Registration list screen

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,16 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent {
+  registerForm!: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder) {
+    this.createForm();
   }
 
+  get nameNoValid() {
+    return this.registerForm.get('name')?.valid;
+  }
+
+  get emailNoValid() {
+    return this.registerForm.get('email')?.valid;
+  }
+
+  get passNoValid() {
+    return this.registerForm.get('password')?.valid;
+  }
+
+  createForm() {
+    this.registerForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    });
+  }
+
+  createUser() {}
 }
