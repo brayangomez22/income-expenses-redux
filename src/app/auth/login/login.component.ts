1e4910aa837a26bf99f8ee14fa2461ebd037598d
brayangomez08 <gomezmancobrayanalexander@gmail.com>
2021-09-22 21:16:50 -0500

    Login of our application

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,16 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
+  loginForm!: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private fb: FormBuilder,
+    private _authService: AuthService,
+    private router: Router
+  ) {
+    this.createForm();
   }
 
+  get emailNoValid() {
+    return this.loginForm.get('email')?.valid;
+  }
+
+  get passNoValid() {
+    return this.loginForm.get('password')?.valid;
+  }
+
+  createForm() {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    });
+  }
+
+  loginUser() {
+    if (this.loginForm.invalid) {
+      return;
+    }
+
+    const { email, password } = this.loginForm.value;
+
+    this._authService
+      .loginUser(email, password)
+      .then((credentials) => this.router.navigate(['/']))
+      .catch((err) => console.error(err));
+  }
 }
