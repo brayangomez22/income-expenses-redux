b2df7a4cce911295f94dd1c59b339f5ecf7459b8
brayangomez08 <gomezmancobrayanalexander@gmail.com>
2021-09-24 16:02:19 -0500

    Screen to capture income and expenses

--- src/app/income-expenses/income-expenses.component.ts
+++ src/app/income-expenses/income-expenses.component.ts
@@ -1,16 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-income-expenses',
   templateUrl: './income-expenses.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class IncomeExpensesComponent implements OnInit {
+  incomeForm!: FormGroup;
+  type: string = 'income';
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.createForm();
   }
 
+  createForm() {
+    this.incomeForm = this.fb.group({
+      description: ['', Validators.required],
+      monto: ['', Validators.required],
+    });
+  }
+
+  save() {
+    if (this.incomeForm.invalid) {
+      return;
+    }
+  }
 }
