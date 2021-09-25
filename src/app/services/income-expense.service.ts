a71a0014006c64cbbdaf29b006fd7d8ce46b9cf5
brayangomez08 <gomezmancobrayanalexander@gmail.com>
2021-09-24 20:24:32 -0500

    Get the UID of our Firebase items

--- src/app/services/income-expense.service.ts
+++ src/app/services/income-expense.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 
 import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { map } from 'rxjs/operators';
 
 import { IncomeExpenses } from '../models/income-expenses.model';
 import { AuthService } from './auth.service';
@@ -25,7 +26,15 @@ export class IncomeExpenseService {
   initIncomeExpensesListener(uid: string) {
     this.firestore
       .collection(`${uid}/income-expense/items`)
-      .valueChanges()
+      .snapshotChanges()
+      .pipe(
+        map((snapshot) =>
+          snapshot.map((doc) => ({
+            uid: doc.payload.doc.id,
+            ...(doc.payload.doc.data() as any),
+          }))
+        )
+      )
       .subscribe((pepe) => console.log(pepe));
   }
 }
