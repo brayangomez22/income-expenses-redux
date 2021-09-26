51fd9847ca76045fcf48dae5fcbe742d1c9a4546
brayangomez08 <gomezmancobrayanalexander@gmail.com>
2021-09-25 23:09:56 -0500

    The application is finished

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,16 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+import { Store } from '@ngrx/store';
+import { AppState } from '../../app.reducer';
+
+import { Subscription } from 'rxjs';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements OnInit, OnDestroy {
+  name: string | undefined = '';
+  userSubs!: Subscription;
 
-  constructor() { }
+  constructor(private store: Store<AppState>) {}
 
   ngOnInit(): void {
+    this.userSubs = this.store
+      .select('user')
+      .pipe(filter(({ user }) => user != null))
+      .subscribe(({ user }) => (this.name = user?.name));
   }
 
+  ngOnDestroy(): void {
+    this.userSubs.unsubscribe();
+  }
 }
